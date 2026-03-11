8a41305ba7aff589e22820c2c5779a0ced5c658c
DoungUk <myyds0723@gmail.com>
2026-03-11 21:39:24 +0900

    feat: LoggingFilter 및 Mdc 상수, LoggingConfig 추가

---
+++ src/main/java/com/ieum/global/logging/MdcConst.java
@@ -0,0 +1,11 @@
+package com.ieum.global.logging;
+
+public class MdcConst {
+    public static final String TRACE_ID = "traceId";
+    public static final String METHOD = "method";
+    public static final String URI = "uri";
+    public static final String CLIENT_IP = "clientIp";
+
+    public static final String MEMBER_ID = "memberId";
+    public static final String ROLE = "role";
+}
