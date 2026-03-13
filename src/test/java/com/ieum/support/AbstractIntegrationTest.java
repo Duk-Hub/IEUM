f23537f4b7db2b57890e8a61a18848601ec6a1b5
DoungUk <myyds0723@gmail.com>
2026-03-13 17:23:55 +0900

    test: 통합테스트, Redis 테스트용 부모 컨테이너 생성

---
+++ src/test/java/com/ieum/support/AbstractIntegrationTest.java
@@ -0,0 +1,36 @@
+package com.ieum.support;
+
+import com.redis.testcontainers.RedisContainer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+public abstract class AbstractIntegrationTest {
+
+    @Container
+    static final MySQLContainer<?> mysql =
+            new MySQLContainer<>("mysql:8.0")
+                    .withDatabaseName("ieum_test")
+                    .withUsername("test")
+                    .withPassword("test");
+
+    @Container
+    static final RedisContainer redis =
+            new RedisContainer(RedisContainer.DEFAULT_IMAGE_NAME.withTag("7"));
+
+    @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port", redis::getFirstMappedPort);
+    }
+}
